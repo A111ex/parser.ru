145026b9916770450ad67ca7d757e8ca828a0bc7
A111ex <MasterV12@ya.ru>
2015-11-25 09:39:24 +0400

    2015-11-25.1

--- config/web.php
+++ config/web.php
@@ -85,7 +85,7 @@ $config = [
 if (YII_ENV_DEV || true) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
+//    $config['modules']['debug'] = 'yii\debug\Module';
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = 'yii\gii\Module';
